fd47642f3215a5a03ac3d96b095902c167b0c509
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-26 16:59:39 +0200

    dodanie animacji

--- js/slajdy.js
+++ js/slajdy.js
@@ -8,8 +8,33 @@ $(function () {
     var slideWidth = 100 / slideCount;
     var slideIndex = 0;
 
+
+    //funkcja animujące, której parametrem jest nwoy index slidu
+    function slide(newSlideIndex) {
+        if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
+            return;
+        }
+        var slideCaption = $(".slider-caption").eq(newSlideIndex);
+        slideCaption.hide();
+
+        var marginLeft = (newSlideIndex * (-100)) + "%";
+
+        //wywałanie animacji na elemencie slideShow, która przesunie go
+        slideShow.animate({
+                "margin-left": marginLeft
+            },
+            800,
+            function () {
+                slideIndex = newSlideIndex;
+                slideCaption.fadeIn("slow")
+            }
+        );
+    }
+
     //szerokość kontenera slideshow
     slideShow.css("width", slideCount * 100 + "%");
+
+
     //iteracja po wszystkich slajdach i nadanie im marginesów i szerokości
     slideShow.find(".single-slide").each(function (index) {
         $(this).css({
